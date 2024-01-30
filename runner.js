d87047d4c29332eef38699bb1c5e102880e780c0
Willames Vital <112904484+WillamesVital@users.noreply.github.com>
2024-01-30 15:55:39 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,12 +1,14 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImMxNjkxZjBhLTBjNzQtNDBhOS04NjhjLTIzODZmOWUxZWM5MC0xNzA2NjQwNDEzMjIxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZDE5MzVlOTgtYWRkZi00ZGZlLWEzZWYtMTM0NTc5YjNiMWMwIiwidHlwZSI6InQifQ.fEPygyIPIPYpy1fTzFGb2K61c98Kw3zkLpPMW4OOwIQ'
+
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: 'token',
+    target: TOKEN,
   }
   tesults.results(results, args);
 })
